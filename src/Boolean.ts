8ceaaab70ab4a5a3936274bcd519f1ea3340e825
Markus Mauch <markus.mauch@gmail.com>
2017-03-15 14:14:05 +0100

    Add files via upload

---
+++ src/Boolean.ts
@@ -0,0 +1,31 @@
+import { Res } from "Sys/Res"
+
+declare global
+{
+    interface BooleanConstructor
+    {
+        /**
+         * Converts a string representation of a logical value to its Boolean object equivalent.
+         * This function is static and can be invoked without creating an instance of the object.
+         * 
+         * @param value
+         *      A string representation of true or false.
+         * @returns
+         *      A Boolean value (true or false) that corresponds to the value argument.
+         */
+        parse( value: string ): boolean;
+    }
+}
+
+function BooleanExtensions()
+{
+    Boolean.parse = ( value: string ) =>
+    {
+        let v = value.trim().toLowerCase();
+        if ( v === "false" ) return false;
+        if ( v === "true" ) return true;
+        throw Error.argumentOutOfRange( "value", value, Res.boolTrueOrFalse );
+    }
+}
+
+export { BooleanExtensions }
